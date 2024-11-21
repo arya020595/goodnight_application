0bf6651d53781c1ef4b19e97255e2ae6ca27a4f0
arya020595 <aryarifqipratama@gmail.com>
2024-11-21 22:36:46 +0700

    Add User, Follow, SleepRecord models and associations

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,2 +1,6 @@
 class SleepRecord < ApplicationRecord
+  belongs_to :user
+
+  validates :clock_in, presence: true
+  validates :clock_out, presence: true, comparison: { greater_than: :clock_in }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :follows, foreign_key: :follower_id
+  has_many :followed_users, through: :follows, source: :followed
+  has_many :followers, through: :follows, source: :follower
+  has_many :sleep_records
+
+  validates :name, presence: true
 end
