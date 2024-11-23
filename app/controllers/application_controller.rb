aed21321f28ab6124fb54b0ca31efe758b30e5b7
arya020595 <aryarifqipratama@gmail.com>
2024-11-23 11:56:24 +0700

    create helper to convert seconds into hours and minutes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  # Helper to convert seconds into hours and minutes
+  def format_duration_in_hours_and_minutes(duration)
+    hours = (duration / 3600).floor
+    minutes = ((duration % 3600) / 60).round
+    { hours: hours, minutes: minutes }
+  end
 end
